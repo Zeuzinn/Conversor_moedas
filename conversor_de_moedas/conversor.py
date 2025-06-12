c2f30a58915d3f7861a53a58420edd7138efac6e
Eliseu Rodrigues <eliseurodrigues37@gmail.com>
2025-06-12 14:17:01 -0300

    feat: adiciona classe ConversorMoeda com suporte a moedas e API

--- conversor_de_moedas/conversor.py
+++ conversor_de_moedas/conversor.py
@@ -1,64 +1,54 @@
 import requests
+from .data_hora import Horario
 
 
-def moeda_usd(reais: float, dados:dict):
-    dolar = float(dados['USDBRL']['bid'])
-    return reais / dolar
-
-
-def moeda_eur(reais: float, dados:dict):
-    euro = float(dados['EURBRL']['bid'])
-    return reais / euro
-
-
-def moeda_btc(reais: float, dados:dict):
-    btc = float(dados['BTCBRL']['bid']) # Bitcoin
-    return reais / btc 
-
-
-def obter_valor():
-    try:
-        valor = float(input("Valor em reais: "))
-        if valor < 0:
-            print("O valor para conversão deve ser maior que R$0,00 \n")
-            return
-        
-        return valor    
-    except ValueError:
-        print("Erro: Deve ser inserido apenas o valor.\n")
-        return None
+class ConversorMoeda:
+    def __init__(self):
+        self._taxas = {}
+        self.atualizar_taxas()
     
-
-def main():
-    while True:
-        # Sempre que repetir o loop, atualiza o conversor com os dados mais recentes da API
+    def atualizar_taxas(self):
         url = "http://economia.awesomeapi.com.br/json/last/USD-BRL,EUR-BRL,BTC-BRL"
         resposta = requests.get(url)
         dados = resposta.json()
-
-        print("\n=== CONVERSOR DE MOEDAS ===\n")    
-        reais = obter_valor()
-        if reais is None:
-            continue
         
-        conversor = input("Converter para (USD, EUR, BTC...): ").upper()
+        # Armazena apenas os valores de 'bid' como float
+        self._taxas = {
+            'USD': float(dados['USDBRL']['bid']),
+            'EUR': float(dados['EURBRL']['bid']),
+            'BTC': float(dados['BTCBRL']['bid']),
+        }
 
-        if conversor == "USD":
-            convertido = moeda_usd(reais, dados)
-            print(f"R${reais:.2f} - ${convertido:.2f}")
-
-        elif conversor == "EUR":
-            convertido = moeda_eur(reais, dados)
-            print(f"R${reais:.2f} - €{convertido:.2f}")   
-
-        elif conversor == "BTC":
-            convertido = moeda_btc(reais, dados)
-            print(f"R${reais:.2f} - Bitcoin:{convertido:.6f}")
-
+    def converter(self, valor_em_reais: float, moeda: str):
+        moeda = moeda.upper()
+        if moeda not in self._taxas:
+            # Lança um erro se a sigla da moeda não existir
+            raise ValueError(f"Moeda '{moeda}' não suportada.") 
+        
+        horario = Horario()
+        hora_atual, data_atual= horario.obter_horario()
+        
+        taxa = self._taxas[moeda]
+        convertido = valor_em_reais / taxa
+        simbolo = self._cifrao(moeda)
+        
+        if moeda == 'BTC':
+            return (
+                f"Data: {data_atual} | Horário: {hora_atual}\n"
+                f"R${valor_em_reais:.2f} - Convertido: {simbolo}{convertido:.6f}"
+        )
         else:
-            print("Moeda não suportada no momento.")
+            return (
+                f"Data: {data_atual} | Horário: {hora_atual}\n"
+                f"R${valor_em_reais:.2f} - Convertido: {simbolo}{convertido:.2f}"
+        )
+    
+    def _cifrao(self, moeda:str):
+        simbolos = {
+        'USD': '$',
+        'EUR': '€',
+        'BTC': '₿'
+    }
+        simbolo = simbolos.get(moeda, '')
         
-        opcao = input("Deseja converter outro valor? (S/N): ").strip().upper()
-        if opcao != "S":
-            print("Programa encerrado.")
-            break
+        return simbolo
